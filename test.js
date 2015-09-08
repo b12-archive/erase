9160a5b74476f88ae79bd05f07e89b4fbc975fc8
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-08 13:41:49 +0200

    Write tests for tag pairs

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
 const test = require('tape-catch');
+
+test('Takes pairs of tags', (is) => {
+  const single = {
+    actual: erase('a<!-- @erase start -->b<!-- @erase end -->c'),
+    expected: 'ac',
+  };
+
+  is.equal(single.actual, single.expected,
+    'a single pair'
+  );
+
+  const more = {
+    actual: erase(
+      'a<!-- @erase start -->b<!-- @erase end -->' +
+      'c<!-- @erase start -->d<!-- @erase end -->e'
+    ),
+    expected: 'ace',
+  };
+
+  is.equal(more.actual, more.expected,
+    'more than one pair'
+  );
+
+  const newline = {
+    actual: erase('a\n<!-- @erase start -->b\n<!-- @erase end -->c\n'),
+    expected: 'a\nc\n',
+  };
+
+  is.equal(newline.actual, newline.expected,
+    'doesn’t fear newlines'
+  );
+});
